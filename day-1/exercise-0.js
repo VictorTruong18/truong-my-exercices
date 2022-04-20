e23a01e02685d1376452a29250e443abe511c034
VictorTruong18 <victor.truong18@gmail.com>
2022-04-20 12:19:20 +0200

    Exercise 0

---
+++ day-1/exercise-0.js
@@ -0,0 +1,2 @@
+export const my_sum = (a,b) => ((typeof(a) === "number") && (typeof(b) === "number") ? a + b : 0);
+
---
+++ index.js
@@ -0,0 +1,3 @@
+import {my_sum} from './day-1/exercise-0.js';
+
+console.log(my_sum(10,20));
