f53ab943a1c7486dab9b658a61d5ac88f2aa3545
VictorTruong18 <victor.truong18@gmail.com>
2022-04-20 15:14:56 +0200

    Add tests

--- day-1/exercise-5.js
+++ day-1/exercise-5.js
@@ -1,12 +1,24 @@
-import { my_size_alpha } from "./exercise-4.js"
+import { my_display_alpha } from "./exercise-1.js"
 
 export const my_array_alpha = (str) => {
-    let res = [];
     if (typeof(str) != "string") {
         return []
     }
-    for(let i = 0; i < my_size_alpha(str); i++){
-        res[i] = str[i];
+    let alphabet = my_display_alpha();
+
+    let res = [];
+    var i = 0;
+    while (typeof(str[i]) != "undefined") {
+        var j = 0;
+        while (typeof(alphabet[j]) != "undefined") {
+            if (str[i] == alphabet[j]) {
+                res.push(str[i]);
+                break;
+            }
+            j += 1;
+        }
+        i += 1;
     }
+
     return res;
 } 
--- test/test.js
+++ test/test.js
@@ -19,6 +19,22 @@ describe('exercise-0', function () {
       it('Sums 444+56 = 500', function () {
         assert.equal(my_sum(444,56), 500);
       });
+      it('should return 0 when no values in either one', function () {
+        assert.equal(0, my_sum());
+        assert.equal(0, my_sum(1));
+    });
+    it('should return 0 if not numbers', function () {
+        assert.equal(0, my_sum("toto", "tata"));
+        assert.equal(0, my_sum(undefined, "zizi"));
+        assert.equal(0, my_sum(15, undefined));
+        assert.equal(0, my_sum(undefined, 15));
+    });
+    it('good result', function() {
+        assert.equal(4, my_sum(2, 2));
+        assert.equal(-4, my_sum(-2, -2));
+        assert.equal(1, my_sum(4, -3));
+    });
+
     });
     describe('my_sum_exception', function () {
         it('Wrong input', function () {
@@ -56,6 +72,17 @@ describe('exercise-1', function () {
       it('Counts the right number of letters', function () {
         assert.equal(my_size_alpha("mabite"),6);
       });
+      it ('Should return 0 if not a string', function() {
+        assert.equal(0, my_size_alpha(69));
+        assert.equal(0, my_size_alpha(undefined));
+        assert.equal(0, my_size_alpha(true));
+    });
+    it ('Return number of letters', function() {
+        
+        
+        assert.equal(5, my_size_alpha("5tot5ot5"));
+    });
+
     });
   });
 
@@ -65,7 +92,20 @@ describe('exercise-1', function () {
       it('returns letter', function () {
         assert.deepEqual(['t', 'o', 't', 'o', 't'], my_array_alpha("totot"));
       });
+      it ('Should return empty if not a string', function() {
+        assert.isEmpty(my_array_alpha(69));
+        assert.isEmpty(my_array_alpha(undefined));
+        assert.isEmpty(my_array_alpha(true));
+    })
+    it ('Return letters', function() {
+        assert.isEmpty(my_array_alpha(""));
+        assert.isEmpty(my_array_alpha("1234"));
+        assert.deepEqual(['t', 'o', 't', 'o', 't'], my_array_alpha("totot"))
+        assert.deepEqual(['t', 'o', 't', 'o', 't'], my_array_alpha("5tot5ot5"))
+    })
+
     });
+    
   });
 
   describe('exercise-6', function () {
@@ -73,6 +113,11 @@ describe('exercise-1', function () {
       it('returns letter', function () {
         assert.deepEqual(my_length_array(['t', 'o', 't', 'o', 't']), 5);
       });
+      it ('Return length', function() {
+        assert.equal(0, my_length_array([]))
+        assert.equal(6, my_length_array([1, 2, 3, 4, 5, 6]))
+    })
+
     });
   });
 
@@ -91,6 +136,13 @@ describe('exercise-1', function () {
         it('returns NEUTRAL', function () {
           assert.equal(my_is_posi_neg(0),"NEUTRAL" );
         });
+        it ('Return positive_negative', function() {
+            assert.equal("POSITIF", my_is_posi_neg(69));
+            assert.equal("NEGATIF", my_is_posi_neg(-69));
+            assert.equal("NEUTRAL", my_is_posi_neg(0));
+            assert.equal("POSITIF", my_is_posi_neg());
+        })
+    
     });
   });
 
